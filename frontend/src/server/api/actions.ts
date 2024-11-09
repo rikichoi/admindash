3aa3b1ada347f77a621f61b207a64c9b167a1de6
Riki225 <79812982+Riki225@users.noreply.github.com>
2024-11-09 16:12:49 +1100

    updated endpoint urls

--- frontend/src/server/api/actions.ts
+++ frontend/src/server/api/actions.ts
@@ -1,11 +1,11 @@
 "use server"
 
 export async function getTransactions() {
-    const transactions = (await fetch(`http://${process.env.NEXT_PUBLIC_ENDPOINT_URL}/api/donation/get-stripe-donations`)).json()
+    const transactions = (await fetch(`http://3.128.24.35:5000/api/donation/get-stripe-donations`)).json()
     return transactions
 }
 
 export async function getDonations() {
-    const donations = (await fetch(`http://${process.env.NEXT_PUBLIC_ENDPOINT_URL}/api/donation/get-donations`, { cache: "no-store" })).json()
+    const donations = (await fetch(`http://3.128.24.35:5000/api/donation/get-donations`, { cache: "no-store" })).json()
     return donations
 }
